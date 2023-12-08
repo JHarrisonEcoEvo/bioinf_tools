a140300829119d1a97ab160f0d03b4c7a094f349
Joshua Harrison <joshua.grant.harrison@gmail.com>
2023-12-08 16:52:55 -0700

    fixing bugs

--- tests/test_seq_tools.py
+++ tests/test_seq_tools.py
@@ -1,24 +1,21 @@
 import unittest
-from click.testing import CliRunner
-from bioinf_tools.seq_tools import nucleotide_counter, rev_complement
+from bioinf_tools.seq_tools.nucleotide_counter import nucleotide_counter
+from bioinf_tools.seq_tools.rev_complement import rev_complement
 
+
+# Usage: python -m unittest tests/test_seq_tools.py
 
 class test_seq_tools(unittest.TestCase):
-
-    # Create a CliRunner instance, a Click utility, so that we can test what
-    # happens for specific arguments from STDIN
-    def setUp(self):
-        self.runner = CliRunner()
-
-    def Test_if_nucleotide_counter_returns_correct_list(self):
+    # Remember that test functions need to start with "test" in order for unittest to recognize them.
+    def test_if_nucleotide_counter_returns_correct_list(self):
         """Test that nucleotide_counter returns the correct list."""
-        result = self.runner.invoke(nucleotide_counter, input='-i AAACCGTT')
+        self.assertEqual(nucleotide_counter('AAACCGTT'), [3, 2, 1, 2])
 
-        self.assertEqual(result, [3, 2, 1, 2])
-
-    def Test_if_rev_complement_returns_correct_string(self):
+    def test_if_rev_complement_returns_correct_string(self):
         """Test that rev_complement returns a reverse complement of a DNA sequence."""
         self.assertEqual(rev_complement('AAACCGTT'), 'AACGGTTT')
+
+
 
 
 if __name__ == '__main__':
