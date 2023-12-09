3b28efe5351a036b3bc6f9ccd9dcdb0354aa46ad
Joshua Harrison <joshua.grant.harrison@gmail.com>
2023-12-08 17:07:45 -0700

    first commit

---
+++ seq_tools/seq_tools.py
@@ -0,0 +1,17 @@
+def gc_counter(string:str):
+    """Count the number of characters in a string"""
+
+    GC_prop = 0
+    for i in string:
+        if i == 'G' or i == 'C':
+            GC_prop += 1
+
+    return GC_prop / len(string)
+
+
+def sliding_window(n, test_string):
+    """Create a sliding window of size n and determine GC content in each window"""
+
+    for i in range(0, len(test_string) - n + 1, 1):
+        print(gc_counter(test_string[i:i+n]))
+
